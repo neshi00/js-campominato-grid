40c41335138da0fff185e4dcab7dc9b67a3c18cb
neshi00 <luca.ascione85@gmail.com>
2021-12-19 15:16:48 +0100

    bug numero box

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ function createNewBox(doveLoMetto) {
   const newBox = document.createElement("div");
   newBox.className = "box";
   doveLoMetto.append(newBox);
+  // al click del box, quello specifico box (this)
+  // si colorerà di blu attraverso l'aggiunta di una nuova classe
+  newBox.addEventListener("click", function () {
+    this.classList.toggle("blue");
+  });
 }
 
 // creo il collegamento col DOM con button e container
 const buttonFirst = document.querySelector(".livello-primo");
+const buttonSecond = document.querySelector(".livello-secondo");
+const buttonThird = document.querySelector(".livello-terzo");
 const containerHtml = document.querySelector(".container");
 
 // al click del button associo un ciclo for che al suo interno
@@ -15,7 +22,30 @@ buttonFirst.addEventListener("click", function () {
   // prima del ciclo aggiungo una classe a container con cui determino
   // una grandezza particolare per l'insieme dei blocchi
   containerHtml.classList.add("easy-mode");
-  for (i = 0; i < 100; i++) {
+  containerHtml.classList.remove("medium-mode");
+  containerHtml.classList.remove("hard-mode");
+
+  for (i = 1; i < 101; i++) {
+    createNewBox(containerHtml);
+  }
+});
+
+buttonSecond.addEventListener("click", function () {
+  containerHtml.classList.remove("easy-mode");
+  containerHtml.classList.add("medium-mode");
+  containerHtml.classList.remove("hard-mode");
+
+  for (i = 0; i < 81; i++) {
+    createNewBox(containerHtml);
+  }
+});
+
+buttonThird.addEventListener("click", function () {
+  containerHtml.classList.remove("easy-mode");
+  containerHtml.classList.remove("medium-mode");
+  containerHtml.classList.add("hard-mode");
+
+  for (i = 0; i < 49; i++) {
     createNewBox(containerHtml);
   }
 });
