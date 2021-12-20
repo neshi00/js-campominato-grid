32098cb83202efcc6c76d62d4c46ea143a3b4780
neshi00 <luca.ascione85@gmail.com>
2021-12-20 12:17:05 +0100

    bug corretto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // creo una funzione per la creazione di nuovi box
-function createNewBox(doveLoMetto) {
+function createNewBox(doveLoCreo) {
   const newBox = document.createElement("div");
   newBox.className = "box";
-  doveLoMetto.append(newBox);
+  doveLoCreo.append(newBox);
   // al click del box, quello specifico box (this)
   // si colorerà di blu attraverso l'aggiunta di una nuova classe
   newBox.addEventListener("click", function () {
-    this.classList.toggle("blue");
+    this.classList.add("blue");
   });
 }
 
@@ -19,8 +19,13 @@ const containerHtml = document.querySelector(".container");
 // al click del button associo un ciclo for che al suo interno
 // va a richiamare la funzione per la creazione di nuovi box.
 buttonFirst.addEventListener("click", function () {
-  // prima del ciclo aggiungo una classe a container con cui determino
+  // prima resetto tutto
+  // poi aggiungo una classe a container con cui determino
   // una grandezza particolare per l'insieme dei blocchi
+  // infine apro un ciclo for che mi crea tanti box quanti ne sono indicati nel ciclo stesso
+
+  containerHtml.innerHTML = "";
+
   containerHtml.classList.add("easy-mode");
   containerHtml.classList.remove("medium-mode");
   containerHtml.classList.remove("hard-mode");
@@ -31,6 +36,8 @@ buttonFirst.addEventListener("click", function () {
 });
 
 buttonSecond.addEventListener("click", function () {
+  containerHtml.innerHTML = "";
+
   containerHtml.classList.remove("easy-mode");
   containerHtml.classList.add("medium-mode");
   containerHtml.classList.remove("hard-mode");
@@ -41,6 +48,7 @@ buttonSecond.addEventListener("click", function () {
 });
 
 buttonThird.addEventListener("click", function () {
+  containerHtml.innerHTML = "";
   containerHtml.classList.remove("easy-mode");
   containerHtml.classList.remove("medium-mode");
   containerHtml.classList.add("hard-mode");
